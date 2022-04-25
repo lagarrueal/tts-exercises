d67a0d2d36d52b2de7726ae19b256db758f996dc
Atli <atli@eleganza.home>
2022-04-25 11:16:03 +0100

    added info for example.py in assignment 1

--- 1_digital_audio/example.py
+++ 1_digital_audio/example.py
@@ -4,6 +4,10 @@ from tools import read_audio, save_audio
 def test():
     # wave contains the samples and sr is the sample rate
     # for the audio file
+
+    # note: On some systems, this relative path will not work
+    #       and you will need to change it to an absolute path
+    #       (e.g. /path/to/data/f1.wav)
     wave, sr = read_audio('./data/f1.wav')
     print(f'The number of samples: {wave.shape[0]}\n')
 
