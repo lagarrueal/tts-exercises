db4bed8a32b04e291422f5c02936a615e4c7e36f
Alexandre Lagarrue <lagarrueal@cy-tech.fr>
2022-04-27 21:20:33 +0000

    Assignment 2 DONE

--- 2_audio_manipulation/template.py
+++ 2_audio_manipulation/template.py
@@ -82,7 +82,6 @@ def fade(wave, start_val, stop_val):
     gradient = np.linspace(start_val, stop_val, wave.shape[0])
     for i in range(wave.shape[0]):
         wave[i] *= gradient[i]
-    
     return wave
     
 def main():
@@ -121,27 +120,27 @@ def main():
     # output for sr, sr/2, sr/4, sr/8, sr/16. sr/32.
     # sr
     wave3,sr3 = read_audio('./data/m2.wav')
-    print(sr3)
+    print(f"sr {sr3}")
     # sr/2
     wave4,sr4 = half_sr(wave3), sr3//2
     save_audio(wave4, sr4, './data/sr_over_2_m2.wav')
-    print(sr4)
+    print(f"sr/2 {sr4}")
     # sr/4
     wave5,sr5 = half_sr(wave4), sr4//2
     save_audio(wave5, sr5, './data/sr_over_4_m2.wav')
-    print(sr5)
+    print(f"sr/4 {sr5}")
     # sr/8
     wave6,sr6 = half_sr(wave5), sr5//2
     save_audio(wave6, sr6, './data/sr_over_8_m2.wav')
-    print(sr6)
+    print(f"sr/8 {sr6}")
     # sr/16
     wave7,sr7 = half_sr(wave6), sr6//2
     save_audio(wave7, sr7, './data/sr_over_16_m2.wav')
-    print(sr7)
+    print(f"sr/16 {sr7}")
     # sr/32
     wave8,sr8 = half_sr(wave7), sr7//2
     save_audio(wave8, sr8, './data/sr_over_32_m2.wav')
-    print(sr8)
+    print(f"sr/32 {sr8}")
     
     # ziping waves
     wave9,sr9 = read_audio('./data/f1.wav')
@@ -162,11 +161,66 @@ def main():
     
     #fading waveform
     wave11, sr11 = read_audio('./data/m2.wav')
-    fade_wave = fade(wave11, 5, 0.01)
+    fade_wave = fade(wave11, 0, 1)
     plt.plot(fade_wave)
     plt.title("Fade wave")
     plt.show()
     save_audio(fade_wave, sr11, './data/fade.wav')
-        
+
+    #4: COMBINE
+    # 1. Open f1.wav and m2.wav
+    # 2. Take the first 0 to 3 seconds of f1.wav and m2.wav
+    # 3. Mix the segments together, with a coefficient of  
+    #    2 for f1.wav and 0.25 for m2.wav
+    # 4. Same step as 3. but with a coefficient of 0.25 
+    #    for f1.wav and 2 for m2.wav
+    # 5. Take the same segments from f1.wav and m2.wav and 
+    #    zip them together while using half the sample rate
+    #    so the sound is not distorded
+    # 6. Fade f1.wav from 0 to 2 and m2.wav from 0 to 0
+    # 7. Concatenate the different waves together to be    
+    #    able to hear each transformation separatly
+    # 8. Save the result as combination_f1m2.wav in ./data/
+    wave_f1, rs_f1 = read_audio('./data/f1.wav')
+    wave_m2, rs_m2 = read_audio('./data/m2.wav')
+    
+    f1_start1 = int (0 * rs_f1)
+    f1_end1 = int (3 * rs_f1)
+    f1_segment1 = wave_f1[f1_start1:f1_end1]
+    m2_start1 = int (0 * rs_m2)
+    m2_end1 = int (3 * rs_m2)
+    m2_segment1 = wave_m2[m2_start1:m2_end1]
+    first_mix = mix_waves(f1_segment1, m2_segment1, 2, 0.25)
+    f1_start2 = int (0 * rs_f1)
+    f1_end2 = int (3 * rs_f1)
+    f1_segment2 = wave_f1[f1_start2:f1_end2]
+    m2_start2 = int (0 * rs_m2)
+    m2_end2 = int (3 * rs_m2)
+    m2_segment2 = wave_m2[m2_start2:m2_end2]
+    second_mix = mix_waves(f1_segment2, m2_segment2, 0.25, 2 )
+    
+    f1_start3 = int (0 * rs_f1 * 2)
+    f1_end3 = int (3 * rs_f1 * 2)
+    f1_segment3 = wave_f1[f1_start3:f1_end3]
+    m2_start3 = int (0 * rs_m2 * 2)
+    m2_end3 = int (3 * rs_m2 * 2)
+    m2_segment3 = wave_m2[m2_start3:m2_end3]
+
+    ziped = zip_waves(half_sr(f1_segment3), half_sr(m2_segment3))
+    
+    fade_f1 = fade(wave_f1, 0, 2)
+    fade_m2 = fade(wave_m2, 2, 0)
+    
+    final_wave = np.concatenate((first_mix, second_mix, ziped, fade_f1, fade_m2))
+
+
+    
+    plt.plot(final_wave)
+    plt.title("Ziped and faded segments from f1.wav and m2.wav")
+    plt.show()
+    save_audio(final_wave, sr11, './data/combination_f1m2.wav')
+    
+    
+
 if __name__ == '__main__':
     main()
